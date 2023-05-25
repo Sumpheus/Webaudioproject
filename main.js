6d1dd204712ce84569542436e9dcb29a10cb919b
Kevin Nguma <maester.jax@gmail.com>
2023-05-25 15:05:58 +0200

    player test

---
+++ main.js
@@ -0,0 +1,14 @@
+const audio = document.querySelector("audio");
+
+const playBtn = document.querySelector(".play");
+const pauseBtn = document.querySelector(".pause");
+
+playBtn.addEventListener("click", () => {
+    audio.play();
+})
+
+pauseBtn.addEventListener("click", () => {
+    audio.pause();
+})
+
+console.log(audio);
